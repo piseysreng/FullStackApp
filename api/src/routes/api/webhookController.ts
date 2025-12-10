d71b75e5466cf82cbf7d0f8a8d5fe2eb96f2d0bd
piseysreng <pisey.sreng@gmail.com>
2025-12-10 10:44:16 +0700

    5 Commit

--- api/src/routes/api/webhookController.ts
+++ api/src/routes/api/webhookController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { verifyWebhook } from '@clerk/express/webhooks'
 import { db } from '../../db/index.js';
 import { eq } from "drizzle-orm";
+import { userTable } from "src/db/oldSchema.js";
 
 export async function clerkRegisterUser(req: Request, res: Response) {
     try {
@@ -14,9 +15,16 @@ export async function clerkRegisterUser(req: Request, res: Response) {
         // console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
         // console.log('Webhook payload:', evt.data)
         if (evt.type === 'user.created') {
-            console.log('userId:', evt.data.id);
+            // Create User in Database
+            try {
+                const [user] = await db.insert(userTable).values({
+                    clerkId: evt.data.id
+                }).returning();
+                res.status(201).json({message: "Create User Successfully"});
+            } catch (error) {
+                res.status(500).send(error);
+            }
         }
-
         return res.send('Webhook received')
     } catch (err) {
         console.error('Error verifying webhook:', err)
